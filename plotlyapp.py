27241451b9bfe55eb65696e243fe478be6ddc6e0
Jordan Castelino <jordan.castelino@gmail.com>
2025-04-02 02:00:44 +0000

    internal ip in dash app

--- plotlyapp.py
+++ plotlyapp.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 from dash import html, Dash
+import socket
 
-app = Dash()
+ip = socket.gethostbyname(socket.gethostname())
+
+app = Dash(server=True,name='app1')
 
 app.layout = [html.Div(children="Hello")]
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8000,host=ip)
